2d8b4d798304c5a5599356b355ced8dbb4b63fff
SG <2767525216@qq.com>
2023-02-12 22:40:41 +0800

    JS/手写call.js

--- JS/手写call.js
+++ JS/手写call.js
@@ -1,3 +1,22 @@
 /**
  * 借用Symbol防止污染到全局属性
  */
+Function.prototype.demoCall = function (context) {
+    if(typeof this !== 'function') {
+        return undefined
+    }
+    context = context || globalThis
+    const fn = Symbol()
+    context[fn] = this
+    const args = [...arguments].slice(1)
+    const result = context[fn](...args)
+    delete context[fn]
+    return result 
+}
+
+function fn(a, b)  {
+    return a + b
+}
+
+const res = fn.demoCall(undefined,1,2)
+console.log(res);
