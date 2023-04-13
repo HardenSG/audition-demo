4a16a73a4697c41dd56bcbd356722d97195c34b1
SG <2767525216@qq.com>
2023-04-13 18:59:34 +0800

    Data_Struct环境init

---
+++ Data_Struct/src/双向链表.ts
@@ -0,0 +1,10 @@
+class 节点 {}
+
+export class DoublyLinkedList {
+  head: DoublyLinkedList | null = null;
+  tail: DoublyLinkedList | null = null;
+}
+
+const a = new DoublyLinkedList();
+
+console.log(a);
